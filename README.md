56b63b846dd729648d3dda7b8daef10ca861be78
emshen6 <parfenovao752@gmail.com>
2024-11-16 20:02:10 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BIV-hackathon
+
+https://drive.google.com/file/d/1gWDHP381W1Sy-TOTLphdap029gsQHyO3
---
+++ test.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import re
+import nltk
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+from nltk.corpus import stopwords
+from pymorphy3 import MorphAnalyzer
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('russian'))
+morph = MorphAnalyzer()
+
+class ResultDataset(Dataset):
+    def __init__(self, encodings):
+        self.encodings = encodings
+    
+    def __len__(self):
+        return len(self.encodings['input_ids'])
+    
+    def __getitem__(self, idx):
+        return {
+            'input_ids': torch.tensor(self.encodings['input_ids'][idx], dtype=torch.long),
+            'attention_mask': torch.tensor(self.encodings['attention_mask'][idx], dtype=torch.long)
+        }
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^а-яё\s]', '', text)
+    words = [word for word in text.split() if word not in stop_words]
+    words = [morph.normal_forms(word)[0] for word in words]
+    return ' '.join(words)
+
+def remove_single_letters(text):
+    return re.sub(r'\b[а-яё]\b', '', text)
+
+data = pd.read_csv('./payments_main.tsv', sep='\t', header=None)
+data.columns = ['id', 'date', 'sum', 'text']
+
+data['text'] = data['text'].apply(preprocess_text)
+data['text'] = data['text'].apply(remove_single_letters)
+
+model = AutoModelForSequenceClassification.from_pretrained("./ruBert_tiny", num_labels=9)
+tokenizer = AutoTokenizer.from_pretrained("./ruBert_tiny")
+
+device = torch.device('cpu')
+model.to(device)
+
+encodings = tokenizer(list(data['text']), truncation=True, padding=True, max_length=181)
+dataset = ResultDataset(encodings)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+
+predicted_labels = []
+
+model.eval()
+with torch.no_grad():
+    for batch in tqdm(dataloader, desc="Processing batches"):
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        preds = torch.argmax(logits, dim=1)
+        predicted_labels.extend(preds.cpu().numpy())
+
+encoder = {'BANK_SERVICE': 0, 'FOOD_GOODS': 1, 'LEASING': 2, 'LOAN': 3, 'NON_FOOD_GOODS': 4, 'NOT_CLASSIFIED': 5, 'REALE_STATE': 6, 'SERVICE': 7, 'TAX': 8}
+decoder = {v: k for k, v in encoder.items()}
+
+decoded_labels = [decoder[label] for label in predicted_labels]
+
+data['label'] = decoded_labels
+
+data.to_csv('submission.tsv', sep='\t', index=False)
